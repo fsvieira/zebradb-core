7ae231ce0b6c6686463863e71651558fdcbf3575
Filipe Vieira <sv.filipe@gmail.com>
2015-07-28 21:45:19 +0000

    Fix not equal bugs, add set example.

--- test/not.js
+++ test/not.js
@@ -107,22 +107,39 @@ describe('ZQuery Tests.', function() {
 
             should(utils.tableFieldsToString(
                 query("(distinct (color 'a) (color 'b))")
-            )).eql({});
-
-            
-            // should(run.queryArray("(distinct (color 'a) (color 'b))")).eql([
-            //    "(distinct (color 'a = blue) (color 'b = yellow))",
-            //    "(distinct (color 'a = red) (color 'b = yellow))",
-            //    "(distinct (color 'a = yellow) (color 'b = blue))",
-            //    "(distinct (color 'a = red) (color 'b = blue))",
-            //    "(distinct (color 'a = yellow) (color 'b = red))",
-            //    "(distinct (color 'a = blue) (color 'b = red))"
-            // ]);
+            )).eql({
+                query: '?(distinct (color \'a) (color \'b))',
+                result: [
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'blue', b: 'yellow', item: '(color \'a)', x$0: '(color \'b)' }
+                    },
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'red', b: 'yellow', item: '(color \'a)', x$0: '(color \'b)' }
+                    },
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'yellow', b: 'blue', item: '(color \'a)', x$0: '(color \'b)' }
+                    },
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'red', b: 'blue', item: '(color \'a)', x$0: '(color \'b)' }
+                    },
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'yellow', b: 'red', item: '(color \'a)', x$0: '(color \'b)' }
+                    },
+                    {
+                      bound: [ 'a', 'b', 'item', 'x$0' ],
+                      vars: { a: 'blue', b: 'red', item: '(color \'a)', x$0: '(color \'b)' }
+                    }
+                ]
+            });
         });
 
         it('Should declare a Set (inv)', function () {
-            var run;
-            run = new ZQuery.Run(
+            var query = Z.run(
                 "(number 0)" +
                 "(number 1)" +
                 "(set)" +
@@ -130,40 +147,248 @@ describe('ZQuery Tests.', function() {
                 "(set ^'item (set 'item 'tail ') (set ^'item 'tail '))"
             );
             
-            should(run.queryArray("(set (number 'a) (set (number 'b) (set) ') ')")).eql([
-                "(set (number 'a = 1) (set (number 'b = 0) (set) ') ' = (set ' 'tail = (set) '))",
-                "(set (number 'a = 0) (set (number 'b = 1) (set) ') ' = (set ' 'tail = (set) '))"
-            ]);
+            should(utils.tableFieldsToString(
+                query("(set (number 'a) (set (number 'b) (set) ') ')")
+            )).eql({
+                query: '?(set (number \'a) (set (number \'b) (set) \'x$0) \'x$1)',
+                result: [
+                    {
+                        bound: [
+                            'a',
+                            'b',
+                            'item',
+                            'tail',
+                            'x$0',
+                            'x$1',
+                            'x$2',
+                            'x$3',
+                            'x$4',
+                            'x$5',
+                            'x$6',
+                            'x$7',
+                            'x$8',
+                            'x$9'
+                        ],
+                        vars: {
+                            a: '1',
+                            b: '0',
+                            item: '(number \'b)',
+                            tail: '(set)',
+                            x$0: '',
+                            x$1: '(set \'x$2 \'tail \'x$3)',
+                            x$2: '\'x$8',
+                            x$3: '\'x$9',
+                            x$4: '(number \'a)',
+                            x$5: '\'x$0',
+                            x$6: '\'x$0',
+                            x$7: '(number \'b)',
+                            x$8: '',
+                            x$9: ''
+                        }
+                    },
+                    {
+                        bound: [
+                            'a',
+                            'b',
+                            'item',
+                            'tail',
+                            'x$0',
+                            'x$1',
+                            'x$2',
+                            'x$3',
+                            'x$4',
+                            'x$5',
+                            'x$6',
+                            'x$7',
+                            'x$8',
+                            'x$9'
+                        ],
+                        vars: {
+                            a: '0',
+                            b: '1',
+                            item: '(number \'b)',
+                            tail: '(set)',
+                            x$0: '',
+                            x$1: '(set \'x$2 \'tail \'x$3)',
+                            x$2: '\'x$8',
+                            x$3: '\'x$9',
+                            x$4: '(number \'a)',
+                            x$5: '\'x$0',
+                            x$6: '\'x$0',
+                            x$7: '(number \'b)',
+                            x$8: '',
+                            x$9: ''
+                        }
+                    }
+                ]
+            });
         });
 
         it('Should declare simple not.', function () {
-            var run;
-            run = new ZQuery.Run(
+            var query = Z.run(
                 "(number 0)" +
                 "(number 1)" +
                 "(not 'a ^'a)"
             );
             
-            should(run.queryArray("(not (number 'p) (number 'q))")).eql([
-                "(not (number 'p = 1) (number 'q = 0))",
-                "(not (number 'p = 0) (number 'q = 1))"
-            ]);
+            should(utils.tableFieldsToString(
+                query("(not (number 'p) (number 'q))")
+            )).eql({
+                query: '?(not (number \'p) (number \'q))',
+                result: [
+                    {
+                        bound: [ 'a', 'p', 'q', 'x$0' ],
+                        vars: { a: '(number \'p)', p: '1', q: '0', x$0: '(number \'q)' }
+                    },
+                    {
+                        bound: [ 'a', 'p', 'q', 'x$0' ],
+                        vars: { a: '(number \'p)', p: '0', q: '1', x$0: '(number \'q)' }
+                    }
+                ]
+            });
         });
         
         it('Should declare a Set', function () {
-            var run;
-            run = new ZQuery.Run(
+            var query = Z.run(
                 "(number 0)" +
                 "(number 1)" +
                 "(set)" +
                 "(set 'item (set) ')" +
                 "(set 'item (set ^'item 'tail ') (set 'item 'tail '))"
             );
-            
-            should(run.queryArray("(set (number 'a) (set (number 'b) (set) ') ')")).eql([
-                "(set (number 'a = 0) (set (number 'b = 1) (set) ') ' = (set 'item = (number 'a = 0) 'tail = (set) '))",
-                "(set (number 'a = 1) (set (number 'b = 0) (set) ') ' = (set 'item = (number 'a = 1) 'tail = (set) '))"
-            ]);
+
+            should(utils.tableFieldsToString(
+                query("(set (number 'a) (set (number 'b) (set) ') ')")
+            )).eql({
+                query: '?(set (number \'a) (set (number \'b) (set) \'x$0) \'x$1)',
+                result: [
+                    {
+                        bound: [
+                            'a',
+                            'b',
+                            'item',
+                            'tail',
+                            'x$0',
+                            'x$1',
+                            'x$2',
+                            'x$3',
+                            'x$4',
+                            'x$5',
+                            'x$6',
+                            'x$7',
+                            'x$8'
+                        ],
+                        vars: {
+                            a: '0',
+                            b: '1',
+                            item: '\'x$7',
+                            tail: '(set)',
+                            x$0: '',
+                            x$1: '(set \'item \'tail \'x$2)',
+                            x$2: '\'x$8',
+                            x$3: '(number \'b)',
+                            x$4: '\'x$0',
+                            x$5: '\'x$0',
+                            x$6: '(number \'b)',
+                            x$7: '(number \'a)',
+                            x$8: ''
+                        }
+                    },
+                    {
+                        bound: [
+                            'a',
+                            'b',
+                            'item',
+                            'tail',
+                            'x$0',
+                            'x$1',
+                            'x$2',
+                            'x$3',
+                            'x$4',
+                            'x$5',
+                            'x$6',
+                            'x$7',
+                            'x$8'
+                        ],
+                        vars: {
+                            a: '1',
+                            b: '0',
+                            item: '\'x$7',
+                            tail: '(set)',
+                            x$0: '',
+                            x$1: '(set \'item \'tail \'x$2)',
+                            x$2: '\'x$8',
+                            x$3: '(number \'b)',
+                            x$4: '\'x$0',
+                            x$5: '\'x$0',
+                            x$6: '(number \'b)',
+                            x$7: '(number \'a)',
+                            x$8: ''
+                        }
+                    }
+                ]
+            });
         });
+        
+        it('Should declare a number Set', function () {
+            var query = Z.run(
+                "(number 0)" +
+                "(number 1)" +
+                "(set)" +
+                "(set (number 'a) (set) ')" +
+                "(set (number 'a) (set (number ^'a) 'tail ') (set (number 'a) 'tail '))"
+            );
+
+            should(utils.tableFieldsToString(
+                query("(set (number 'a) 'tail ')")
+            )).eql({
+                query: '?(set (number \'a) \'tail \'x$0)',
+                result: [
+                    {
+                        bound: [ 'a', 'tail', 'x$0', 'x$1', 'x$2' ],
+                        vars: { a: '0', tail: '(set)', x$0: '\'x$2', x$1: '\'a', x$2: '' }
+                    },
+                    {
+                        bound: [ 'a', 'tail', 'x$0', 'x$1', 'x$2' ],
+                        vars: { a: '1', tail: '(set)', x$0: '\'x$2', x$1: '\'a', x$2: '' }
+                    },
+                    {
+                        bound: [ 'a', 'tail', 'x$0', 'x$1', 'x$2', 'x$3', 'x$4', 'x$5', 'x$6', 'x$7', 'x$8', 'x$9'],
+                        vars: {
+                            a: '\'x$3',
+                            tail: '(set (number \'x$5) \'x$4 \'x$1)',
+                            x$0: '(set (number \'x$3) \'x$4 \'x$2)',
+                            x$1: '',
+                            x$2: '1',
+                            x$3: '0',
+                            x$4: '(set)',
+                            x$5: '\'x$2',
+                            x$6: '\'x$1',
+                            x$7: '',
+                            x$8: '\'x$2',
+                            x$9: ''
+                        }
+                    },
+                    {
+                        bound: [ 'a', 'tail', 'x$0', 'x$1', 'x$2', 'x$3', 'x$4', 'x$5', 'x$6', 'x$7', 'x$8', 'x$9'],
+                        vars: {
+                            a: '\'x$3',
+                            tail: '(set (number \'x$5) \'x$4 \'x$1)',
+                            x$0: '(set (number \'x$3) \'x$4 \'x$2)',
+                            x$1: '',
+                            x$2: '0',
+                            x$3: '1',
+                            x$4: '(set)',
+                            x$5: '\'x$2',
+                            x$6: '\'x$1',
+                            x$7: '',
+                            x$8: '\'x$2',
+                            x$9: ''
+                        }
+                    }
+                ]
+            });
+        });
+
     });
 });
