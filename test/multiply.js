e754e119f4c55ac1a234e6f3265e163f98c9da8e
Filipe Vieira <sv.filipe@gmail.com>
2015-10-22 21:27:40 +0000

    add multiply test.

---
+++ test/multiply.js
@@ -0,0 +1,41 @@
+var should = require("should");
+var Z = require("../lib/unify");
+
+
+describe('Multiply Tests', function() {
+    describe('Test Multiply results', function() {
+        it('should multiply results.', function () {
+            var z = new Z.Run(
+                "(yellow 'a)" +
+                "('b blue)"
+            );
+            
+            should(
+                Z.toString(z.query(
+                    "('c 'd)"
+                )
+            )).eql(
+                "(yellow 'a)\n" +
+                "('b blue)\n" +
+                "(yellow blue)"
+            );
+        });
+        
+        it('should multiply results (with variables with same name).', function () {
+            var z = new Z.Run(
+                "(yellow 'a)" +
+                "('a blue)"
+            );
+            
+            should(
+                Z.toString(z.query(
+                    "('a 'b)"
+                )
+            )).eql(
+                "(yellow 'x$0)\n" +
+                "('x$0 blue)\n" +
+                "(yellow blue)"
+            );
+        });
+    });
+});
