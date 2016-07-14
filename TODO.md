a047bc5442ed4db1d1d0c1c0742464d16efd6ef2
Filipe Vieira <sv.filipe@gmail.com>
2016-07-14 16:20:16 +0000

    planner improvements.

--- TODO.md
+++ TODO.md
@@ -1,19 +1,24 @@
-# Roadmap
-
-* 2.0.0 (WIP)
-    * Add support to finit representation of infinity structures (ex: (nat 'x) -> 'x => (nat 'x))
-* 1.0.0 version
-    * Add examples, solve brave puzzle (http://www.mathsisfun.com/puzzles/a-brave-puzzle.html)
-* beta version
-    * Optimize (solve all [current] tests under 20 seconds, optimal < 2s),
-    * ~~Remove duplicated results,~~
-* alfa version (2015-10-22)
-    * ~~Clean up and rename files, functions,~~
-    * ~~Add "multiply" results support,~~
-    * ~~Fix tools,~~
-    * ~~Fix imports and zlib tests,~~
-    * ~~Add ignore "_" term support.~~
-
-
 # TODO:
-    - ~~remove duplicates from multiply~~
+    - Planner
+        - select linked tuples, after selecting initial tuples include tuples with same variables included on initial tuples.
+        - construct dependicy graphs for query:
+            - rank: number of dependecys,
+            - check for linked variables transformation to constants.
+            - check for tree leaf.
+            
+        - construct tree with linked tuples, make virtual linked tuples with definitions, analise dependecys and leafs.
+
+- Problems:
+    - planner can't just choose tuples by looking at definitions,
+    - there must be a choice of related tuples or father tuple validation may take to long before its validated.
+    - also other child tuples may grow to infinity.
+
+
+
+---
+    Virtual Steps:
+        - create definitions graph (with an id for every tuple),
+        - associete a virtual field corresponding to the id tuple id on the graph,
+        - everytime definitions are unified with tuples preserve or merge virtuals,
+        - the virtual structure should give a better information of tuple expansion and a tool for planning.
+        
--- test/factorial.js
+++ test/factorial.js
@@ -2,7 +2,7 @@ var should = require("should");
 var Z = require("../lib/z");
 
 describe('Factorial Parser Tests.', function() {
-    describe('Natural Numbers', function() {
+/*
         it('Should declare ~Peanno numbers', function () {
 
             var run = new Z.Run(
@@ -42,10 +42,12 @@ describe('Factorial Parser Tests.', function() {
                 "(nat (nat 'n))" +
 
                 // a . 0 = a,
-                "(+ (nat 'a) (nat 0) (nat 'a) ')" +
+                "(+ (nat 0) (nat 0) (nat 0) ')" +
+                "(+ (nat (nat 'a)) (nat 0) (nat (nat 'a)) ')" +
+                "(+ (nat 0) (nat (nat 'a)) (nat (nat 'a)) ')" +
 
                 // a . S(b) = a + (a . b)
-                "(+ (nat 'a) (nat (nat 'b)) (nat 'r) (+ (nat 'a) (nat 'b) 'r '))"
+                "(+ (nat (nat 'a)) (nat (nat 'b)) (nat 'r) (+ (nat (nat 'a)) (nat 'b) 'r '))"
             );
 
             var query = function (q, len) {
@@ -76,7 +78,7 @@ describe('Factorial Parser Tests.', function() {
                     "(+ (nat 0) (nat (nat 0)) 'r ')"
                 )
             ).eql(
-                "(+ (nat 0) (nat (nat 0)) (nat (nat 0)) (+ (nat 0) (nat 0) (nat 0) **x$0))"
+                "(+ (nat 0) (nat (nat 0)) (nat (nat 0)) 'x$0)"
             );
 
             // 2 + 3 = 5
@@ -161,10 +163,12 @@ describe('Factorial Parser Tests.', function() {
 
                 // Add
                 // a . 0 = a,
-                "(+ (nat 'a) (nat 0) (nat 'a) ')" +
+                "(+ (nat 0) (nat 0) (nat 0) ')" +
+                "(+ (nat (nat 'a)) (nat 0) (nat (nat 'a)) ')" +
+                "(+ (nat 0) (nat (nat 'a)) (nat (nat 'a)) ')" +
 
                 // a . S(b) = a + (a . b)
-                "(+ (nat 'a) (nat (nat 'b)) (nat 'r) (+ (nat 'a) (nat 'b) 'r '))" +
+                "(+ (nat (nat 'a)) (nat (nat 'b)) (nat 'r) (+ (nat (nat 'a)) (nat 'b) 'r '))" +
 
                 // List
                 "(list)" +
@@ -173,10 +177,12 @@ describe('Factorial Parser Tests.', function() {
 
                 // Mul                    
                 // a . 0 = 0
-                "(* (nat 'a) (nat 0) (nat 0) ')" +
+                "(* (nat 0) (nat 0) (nat 0) ')" +
+                "(* (nat (nat 'a)) (nat 0) (nat 0) ')" +
+                "(* (nat 0) (nat (nat 'a)) (nat 0) ')" +
 
                 // a . S(b) = a + (a . b)
-                "(* (nat 'a) (nat (nat 'b)) 'r (list (+ (nat 'a) 'rm 'r ') (list (* (nat 'a) (nat 'b) 'rm ') (list))))"
+                "(* (nat (nat 'a)) (nat (nat 'b)) 'r (list (+ (nat (nat 'a)) 'rm 'r ') (list (* (nat (nat 'a)) (nat 'b) 'rm ') (list))))"
             );
 
             var query = function (q, len) {
@@ -208,7 +214,7 @@ describe('Factorial Parser Tests.', function() {
                     "(* (nat 0) (nat (nat 0)) 'r ')"
                 )
             ).eql(
-                "(* (nat 0) (nat (nat 0)) (nat 0) (list (+ (nat 0) (nat 0) (nat 0) 'x$0) (list (* (nat 0) (nat 0) (nat 0) **x$1) (list))))"
+                "(* (nat 0) (nat (nat 0)) (nat 0) 'x$0)"
             );
 
             // 1 * 1 = 1
@@ -265,20 +271,23 @@ describe('Factorial Parser Tests.', function() {
                 "(* (nat (nat (nat (nat 0)))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (list (+ (nat (nat (nat (nat 0)))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (+ (nat (nat (nat (nat 0)))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat 0)))))) (+ (nat (nat (nat (nat 0)))) (nat (nat 0)) (nat (nat (nat (nat (nat 0))))) (+ (nat (nat (nat (nat 0)))) (nat 0) (nat (nat (nat (nat 0)))) **x$0)))) (list (* (nat (nat (nat (nat 0)))) (nat (nat 0)) (nat (nat (nat (nat 0)))) (list (+ (nat (nat (nat (nat 0)))) (nat 0) (nat (nat (nat (nat 0)))) 'x$1) (list (* (nat (nat (nat (nat 0)))) (nat 0) (nat 0) **x$2) (list)))) (list))))"
             );
         });
-/*
+*/
         it('Should declare a factorial func', function () {
             this.timeout(1000*60*5);
             var run = new Z.Run(
+                // Nat
                 // Nat
                 "(nat 0)" +
                 "(nat (nat 'n))" +
 
                 // Add
                 // a . 0 = a,
-                "(+ (nat 'a) (nat 0) (nat 'a) ')" +
+                "(+ (nat 0) (nat 0) (nat 0) ')" +
+                "(+ (nat (nat 'a)) (nat 0) (nat (nat 'a)) ')" +
+                "(+ (nat 0) (nat (nat 'a)) (nat (nat 'a)) ')" +
 
                 // a . S(b) = a + (a . b)
-                "(+ (nat 'a) (nat (nat 'b)) (nat 'r) (+ (nat 'a) (nat 'b) 'r '))" +
+                "(+ (nat (nat 'a)) (nat (nat 'b)) (nat 'r) (+ (nat (nat 'a)) (nat 'b) 'r '))" +
 
                 // List
                 "(list)" +
@@ -287,10 +296,12 @@ describe('Factorial Parser Tests.', function() {
 
                 // Mul                    
                 // a . 0 = 0
-                "(* (nat 'a) (nat 0) (nat 0) ')" +
+                "(* (nat 0) (nat 0) (nat 0) ')" +
+                "(* (nat (nat 'a)) (nat 0) (nat 0) ')" +
+                "(* (nat 0) (nat (nat 'a)) (nat 0) ')" +
 
                 // a . S(b) = a + (a . b)
-                "(* (nat 'a) (nat (nat 'b)) 'r (list (+ (nat 'a) 'rm 'r ') (list (* (nat 'a) (nat 'b) 'rm ') (list))))" +
+                "(* (nat (nat 'a)) (nat (nat 'b)) 'r (list (+ (nat (nat 'a)) 'rm 'r ') (list (* (nat (nat 'a)) (nat 'b) 'rm ') (list))))" +
 
                 // 0! = 1
                 "(fac (nat 0) (nat (nat 0)) ')" +
@@ -300,7 +311,7 @@ describe('Factorial Parser Tests.', function() {
             var query = function (q, len) {
                 return Z.toString(run.query(q, len));
             };
-            
+/*
             // fac(0) = 1
             should(
                 query(
@@ -316,7 +327,7 @@ describe('Factorial Parser Tests.', function() {
             ).eql(
                 "(fac (nat (nat 0)) (nat (nat 0)) (list (* (nat (nat 0)) (nat (nat 0)) (nat (nat 0)) (list (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$0) (list (* (nat (nat 0)) (nat 0) (nat 0) **x$1) (list)))) (list (fac (nat 0) (nat (nat 0)) **x$2) (list))))"
             );
-
+*/
             // fac(2) = 2
             should(
                 query(
@@ -325,7 +336,7 @@ describe('Factorial Parser Tests.', function() {
             ).eql(
                 "(fac (nat (nat (nat 0))) (nat (nat (nat 0))) (list (* (nat (nat 0)) (nat (nat (nat 0))) (nat (nat (nat 0))) (list (+ (nat (nat 0)) (nat (nat 0)) (nat (nat (nat 0))) (+ (nat (nat 0)) (nat 0) (nat (nat 0)) **x$0)) (list (* (nat (nat 0)) (nat (nat 0)) (nat (nat 0)) (list (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$1) (list (* (nat (nat 0)) (nat 0) (nat 0) **x$2) (list)))) (list)))) (list (fac (nat (nat 0)) (nat (nat 0)) (list (* (nat (nat 0)) (nat (nat 0)) (nat (nat 0)) (list (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$3) (list (* (nat (nat 0)) (nat 0) (nat 0) **x$4) (list)))) (list (fac (nat 0) (nat (nat 0)) **x$5) (list)))) (list))))"
             );
-
+/*
             // fac(3) = 6
             should(
                 query(
@@ -342,7 +353,6 @@ describe('Factorial Parser Tests.', function() {
                 )
             ).eql(
                 "(fac (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))))))))) (list (* (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))))))))) (list (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat 0)))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat 0)) (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat 0) (nat (nat (nat (nat (nat (nat (nat 0))))))) 'x$113))))))))))))))))))) (list (* (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))) (list (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat 0)))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat 0)) (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat 0) (nat (nat (nat (nat (nat (nat (nat 0))))))) 'x$164))))))))))))) (list (* (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (list (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat (nat 0)))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat (nat (nat (nat (nat 0))))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat 0)) (nat (nat (nat (nat (nat (nat (nat (nat 0)))))))) (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat 0) (nat (nat (nat (nat (nat (nat (nat 0))))))) 'x$191))))))) (list (* (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat (nat 0)) (nat (nat (nat (nat (nat (nat (nat 0))))))) (list (+ (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat 0) (nat (nat (nat (nat (nat (nat (nat 0))))))) 'x$200) (list (* (nat (nat (nat (nat (nat (nat (nat 0))))))) (nat 0) (nat 0) 'x$205) (list)))) (list)))) (list)))) (list)))) (list (fac (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (list (* (nat (nat (nat 0))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (list (+ (nat (nat (nat 0))) (nat (nat (nat (nat (nat 0))))) (nat (nat (nat (nat (nat (nat (nat 0))))))) (+ (nat (nat (nat 0))) (nat (nat (nat (nat 0)))) (nat (nat (nat (nat (nat (nat 0)))))) (+ (nat (nat (nat 0))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat 0))))) (+ (nat (nat (nat 0))) (nat (nat 0)) (nat (nat (nat (nat 0)))) (+ (nat (nat (nat 0))) (nat 0) (nat (nat (nat 0))) 'x$236))))) (list (* (nat (nat (nat 0))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat 0))))) (list (+ (nat (nat (nat 0))) (nat (nat (nat 0))) (nat (nat (nat (nat (nat 0))))) (+ (nat (nat (nat 0))) (nat (nat 0)) (nat (nat (nat (nat 0)))) (+ (nat (nat (nat 0))) (nat 0) (nat (nat (nat 0))) 'x$251))) (list (* (nat (nat (nat 0))) (nat (nat 0)) (nat (nat (nat 0))) (list (+ (nat (nat (nat 0))) (nat 0) (nat (nat (nat 0))) 'x$260) (list (* (nat (nat (nat 0))) (nat 0) (nat 0) 'x$265) (list)))) (list)))) (list)))) (list (fac (nat (nat (nat 0))) (nat (nat (nat 0))) (list (* (nat (nat 0)) (nat (nat (nat 0))) (nat (nat (nat 0))) (list (+ (nat (nat 0)) (nat (nat 0)) (nat (nat (nat 0))) (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$284)) (list (* (nat (nat 0)) (nat (nat 0)) (nat (nat 0)) (list (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$293) (list (* (nat (nat 0)) (nat 0) (nat 0) 'x$298) (list)))) (list)))) (list (fac (nat (nat 0)) (nat (nat 0)) (list (* (nat (nat 0)) (nat (nat 0)) (nat (nat 0)) (list (+ (nat (nat 0)) (nat 0) (nat (nat 0)) 'x$313) (list (* (nat (nat 0)) (nat 0) (nat 0) 'x$318) (list)))) (list (fac (nat 0) (nat (nat 0)) 'x$323) (list)))) (list)))) (list)))) (list))))"
-            );
-        });*/
-    });
+            );*/
+        });
 });
