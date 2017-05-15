33b336e2b00bfec266dbec99a7202c81bbed61f7
Filipe Vieira <sv.filipe@gmail.com>
2017-05-15 15:10:29 +0000

    add correct result to max test

--- test/max.js
+++ test/max.js
@@ -3,6 +3,8 @@ var Z = require("../lib/z");
 
 describe('Get the max solution.', function() {
     it('Declare a number set, get the max number set with all elements.', function() {
+        this.timeout(1000 * 60 * 5);
+                
         var run = new Z();
         
         run.add(
@@ -19,7 +21,14 @@ describe('Get the max solution.', function() {
             "(max (set 'i 'tail 'p) ^(set ' (set 'i 'tail 'p) '))"
         );
         
-        should(run.print("?(max ')")).eql("");
+        should(run.print("?(max ')")).eql(
+            "@(max @(set @(number 0) @(set @(number 1) @(set @(number 2) @(set) ') @(set @(number 1) @(set) ')) @(set @(number 0) @(set @(number 2) @(set) ') @(set @(number 0) @(set) '))))[^!(equal (number 0) (number 1))\n!(equal (number 0) (number 2))\n!(equal (number 1) (number 2))\n!(set ' (set (number 0) (set @(number 1) (set (number 2) @(set) ') (set (number 1) @(set) ')) @(set (number 0) (set (number 2) @(set) ') (set (number 0) @(set) '))) ')\n!(set ' (set (number 0) @(set (number 1) (set (number 2) @(set) ') (set (number 1) @(set) ')) (set @(number 0) (set (number 2) @(set) ') (set (number 0) @(set) '))) ')]\n" +
+            "@(max @(set @(number 0) @(set @(number 2) @(set @(number 1) @(set) ') @(set @(number 2) @(set) ')) @(set @(number 0) @(set @(number 1) @(set) ') @(set @(number 0) @(set) '))))[^!(equal (number 0) (number 1))\n!(equal (number 0) (number 2))\n!(equal (number 2) (number 1))\n!(set ' (set (number 0) (set @(number 2) (set (number 1) @(set) ') (set (number 2) @(set) ')) @(set (number 0) (set (number 1) @(set) ') (set (number 0) @(set) '))) ')\n!(set ' (set (number 0) @(set (number 2) (set (number 1) @(set) ') (set (number 2) @(set) ')) (set @(number 0) (set (number 1) @(set) ') (set (number 0) @(set) '))) ')]\n" +
+            "@(max @(set @(number 1) @(set @(number 0) @(set @(number 2) @(set) ') @(set @(number 0) @(set) ')) @(set @(number 1) @(set @(number 2) @(set) ') @(set @(number 1) @(set) '))))[^!(equal (number 0) (number 2))\n!(equal (number 1) (number 0))\n!(equal (number 1) (number 2))\n!(set ' (set (number 1) (set @(number 0) (set (number 2) @(set) ') (set (number 0) @(set) ')) @(set (number 1) (set (number 2) @(set) ') (set (number 1) @(set) '))) ')\n!(set ' (set (number 1) @(set (number 0) (set (number 2) @(set) ') (set (number 0) @(set) ')) (set @(number 1) (set (number 2) @(set) ') (set (number 1) @(set) '))) ')]\n" +
+            "@(max @(set @(number 1) @(set @(number 2) @(set @(number 0) @(set) ') @(set @(number 2) @(set) ')) @(set @(number 1) @(set @(number 0) @(set) ') @(set @(number 1) @(set) '))))[^!(equal (number 1) (number 0))\n!(equal (number 1) (number 2))\n!(equal (number 2) (number 0))\n!(set ' (set (number 1) (set @(number 2) (set (number 0) @(set) ') (set (number 2) @(set) ')) @(set (number 1) (set (number 0) @(set) ') (set (number 1) @(set) '))) ')\n!(set ' (set (number 1) @(set (number 2) (set (number 0) @(set) ') (set (number 2) @(set) ')) (set @(number 1) (set (number 0) @(set) ') (set (number 1) @(set) '))) ')]\n" +
+            "@(max @(set @(number 2) @(set @(number 0) @(set @(number 1) @(set) ') @(set @(number 0) @(set) ')) @(set @(number 2) @(set @(number 1) @(set) ') @(set @(number 2) @(set) '))))[^!(equal (number 0) (number 1))\n!(equal (number 2) (number 0))\n!(equal (number 2) (number 1))\n!(set ' (set (number 2) (set @(number 0) (set (number 1) @(set) ') (set (number 0) @(set) ')) @(set (number 2) (set (number 1) @(set) ') (set (number 2) @(set) '))) ')\n!(set ' (set (number 2) @(set (number 0) (set (number 1) @(set) ') (set (number 0) @(set) ')) (set @(number 2) (set (number 1) @(set) ') (set (number 2) @(set) '))) ')]\n" +
+            "@(max @(set @(number 2) @(set @(number 1) @(set @(number 0) @(set) ') @(set @(number 1) @(set) ')) @(set @(number 2) @(set @(number 0) @(set) ') @(set @(number 2) @(set) '))))[^!(equal (number 1) (number 0))\n!(equal (number 2) (number 0))\n!(equal (number 2) (number 1))\n!(set ' (set (number 2) (set @(number 1) (set (number 0) @(set) ') (set (number 1) @(set) ')) @(set (number 2) (set (number 0) @(set) ') (set (number 2) @(set) '))) ')\n!(set ' (set (number 2) @(set (number 1) (set (number 0) @(set) ') (set (number 1) @(set) ')) (set @(number 2) (set (number 0) @(set) ') (set (number 2) @(set) '))) ')]"
+        );
     });
 });
 
