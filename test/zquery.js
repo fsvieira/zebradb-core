5c7931e4aae6dc0c96663debdbdc71693aafeb3c
Filipe Vieira <sv.filipe@gmail.com>
2017-06-04 14:13:13 +0000

    get query branch id as a promise

--- test/zquery.js
+++ test/zquery.js
@@ -1,83 +1,80 @@
-var should = require("should");
-var Z = require("../lib/z");
+const should = require("should");
+const Z = require("../lib/z");
+const test = require("../lib/testing/test");
 
 describe("ZQuery Tests.", function () {
-    it("Query with single tuple constant.", function () {
-        var run = new Z();
-        
-        run.add("(yellow)");
-        should(run.print("?(yellow)")).eql("@(yellow)");
-        should(run.print("?('q)")).eql("@(yellow)");
-        should(new Z().print("(yellow) ?('q)")).eql("@(yellow)");
-    });
+    it("Query with single tuple constant.",
+        test(
+            "(yellow)" +
+            "?(yellow)" +
+            "?('q)",
+            // results,
+            "?(yellow):\n" +
+                "\t@(yellow)\n" +
+            "?('q):\n" +
+                "\t@(yellow)"
+        )
+    );
 
-    it('Should identify variables by name.', function () {
-        var run = new Z();
-        
-        run.add("('p 'p)");
-        should(run.print(
-            "?(yellow 'p)"
-        )).eql(
-            "@(yellow yellow)"
-        );
+    it('Should identify variables by name, simple tuple.',
+        test(
+            "('p 'p)" +
+            "?(yellow 'p)",
+            "?(yellow 'p):\n" +
+                "\t@(yellow yellow)"
+        )
+    );
 
-        run = new Z();
-        run.add("('q ) (('q) ('q))");
-        
-        should(run.print(
-            "?((yellow) ('p))"
-        )).eql(
-            "@(@(yellow) @(yellow))"
-        );
-    });
+    it('Should identify variables by name, inner tuples.',
+        test(
+            "('q ) (('q) ('q))" +
+            "?((yellow) ('p))",
+            "?((yellow) ('p)):\n" +
+                "\t@(@(yellow) @(yellow))"
+        )
+    );
 
-    it("Should unify variables with tuple values", function () {
-        var run = new Z();
-        
-        run.add("(blue red yellow)");
-        should(run.print(
-            "?('a 'b 'c)"
-        )).eql(
-            "@(blue red yellow)"
-        );
-    });
+    it('Should unify variables with tuple values.',
+        test(
+            "(blue red yellow)" +
+            "?('a 'b 'c)",
+            "?('a 'b 'c):\n" +
+                "\t@(blue red yellow)"
+        )
+    );
 
-    it("Should unify tuples variables.", function() {
+    it('Should unify simple tuples variables.',
+        test(
+            "('a 'a)" +
+            "?(yellow 'c)",
+            "?(yellow 'c):\n" +
+                "\t@(yellow yellow)"
+        )
+    );
 
-        var run = new Z();
-        
-        run.add("('a 'a)");
-        should(run.print(
-            "?(yellow 'c)"
-        )).eql(
-            "@(yellow yellow)"
-        );
+    it('Should unify simple and inner tuples variables.',
+        test(
+            "('x 'y) (('a 'a))" +
+            "?((yellow 'c))",
+            "?((yellow 'c)):\n" +
+                "\t@(@(yellow yellow))"
+        )
+    );
 
-        run = new Z();
-        run.add("('x 'y) (('a 'a))");
+    it('Should unify inner tuples variables at same level.',
+        test(
+            "(yellow blue) (blue yellow) (('a 'b) ('b 'a))" +
+            "?((yellow 'c) (blue 'd))",
+            "?((yellow 'c) (blue 'd)):\n" +
+                "\t@(@(yellow blue) @(blue yellow))"
+        )
+    );
 
-        should(run.print(
-            "?((yellow 'c))"
-        )).eql(
-            "@(@(yellow yellow))"
-        );
-        
-        run = new Z();
-        run.add("(yellow blue) (blue yellow) (('a 'b) ('b 'a))");
-        
-        should(run.print(
-            "?((yellow 'c) (blue 'd))"
-        )).eql(
-            "@(@(yellow blue) @(blue yellow))"
-        );
-    });
-    
-    it("should handle duplicated definitions.", function () {
-        var run = new Z();
-        
-        run.add("('a) ('a)");
-        
-        should(run.print("?(yellow)")).equal("@(yellow)");
-    });
-    
+    it("should handle duplicated definitions.", 
+        test(
+            "('a) ('a) ?(yellow)",
+            "?(yellow):\n" +
+                "\t@(yellow)"
+        )
+    );
 });
