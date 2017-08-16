7d1870acfb208393d38f1f00cffd0c45cae8800c
Filipe Vieira <sv.filipe@gmail.com>
2017-08-16 20:52:01 +0000

    add func to query

--- tools/run.js
+++ tools/run.js
@@ -1,24 +1,59 @@
 #!/usr/bin/env node
 
-var Z = require("../lib/z");
-var fs = require("fs");
+const Session = require("../lib/z");
+const utils = require("../lib/utils");
+const fs = require("fs");
+
+function readFile (filename) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(filename, function (err, data) {
+           if (err) {
+               reject(err);
+           }
+           else {
+               resolve(data.toString());
+           }
+        });
+    });
+}
 
 
 function run (filename) {
-    var run = new Z();
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            try {
-               console.log(run.print(data.toString())); 
-            }
-            catch (e) {
-                console.log(e);
-            }
+    const session = new Session({
+        readFile
+    });
+    
+    function toString (branchId) {
+        const query = session.zvs.getObject(branchId, session.zvs.data.global("query"));
+        return utils.toString(query, true);
+    }
+    
+    const queryIds = [];
+    
+    session.events.on('query-start', function (queryBranchId) {
+        if (queryIds.indexOf(queryBranchId) === -1) {
+            queryIds.push(queryBranchId);
+            console.log("Query Started => " + toString(queryBranchId));
         }
     });
+    
+    session.events.on('success', function (branchId) {
+        const queryBranchId = session.zvs.getObject(branchId, session.zvs.data.global("queryBranchId")).data;
+        if (queryIds.indexOf(queryBranchId) !== -1) {
+            console.log("Query Success: " + toString(queryBranchId) + " => " + toString(branchId));
+        }
+    });
+    
+    session.events.on('track', function ({id: queryBranchId, actives}) {
+        const index = queryIds.indexOf(queryBranchId);
+        if (actives === 0 && index !== -1) {
+            // query ended remove it from list,
+            queryIds.splice(index, 1);
+            console.log("Query Ended => " + toString(queryBranchId));
+        }
+    });
+    
+    session.add({value: "[" + filename + "]"});
 }
 
 if (process.argv.length === 3) {
