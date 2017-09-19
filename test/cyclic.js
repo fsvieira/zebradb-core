c9fde20b3d1878f314b6aa8a667788008bbe6b5b
Filipe Vieira <sv.filipe@gmail.com>
2017-09-19 21:33:53 +0000

    handle cyclic definitions

---
+++ test/cyclic.js
@@ -0,0 +1,67 @@
+const test = require("../lib/testing/test");
+
+describe('Cyclic Tests', function() {
+    it('should handle cyclic data on multiply.',
+        test(
+            `
+            (! 'q)
+            ('p 'q 'p 'q)
+            ('p 'q (! 'q) (! 'p))
+            ?(! 'q)`
+            ,
+            `?(! 'q): @(! 'q)`
+        )
+    );
+
+    it('should handle cyclic data on multiply (simple).',
+        test(
+            `
+            ('p)
+            ('p ('p))
+            ('p 'p)
+            ?(A 'p)
+            ?('p (A))
+            ?(A A)
+            ?('p 'p)
+            ?('a 'b)`
+            ,
+            `?(A 'p):
+                @(A @(A))
+                @(A A)
+
+            ?('p (A)):
+                @(@(A) @(A))
+                @(A @(A))
+
+            ?(A A): 
+                @(A A)
+
+            ?('p 'p):
+                @('p 'p)
+
+            ?('a 'b):
+                @('p 'p)
+                @('p @('p))
+            `
+        )
+    );
+    
+    it('should handle cyclic data on query.',
+        test(
+            `
+            (! 'q)
+            ('p 'q 'p 'q)
+            ?('p 'q (! 'q) (! 'p))
+            ?(' ' (! A) (! A))
+
+            `
+            ,
+            // TODO: why is this empty ???
+            `?('p 'q (! 'q) (! 'p)):
+                <empty>
+            ?(' ' (! A) (! A)):
+                @(@(! A) @(! A) @(! A) @(! A))
+            `
+        )
+    );
+});
