3a17d6d86c9580c36537d17d08fec8040bfb6c9b
Filipe Vieira <sv.filipe@gmail.com>
2018-01-03 23:00:37 +0000

    add tests task to gulp and code-quality

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,55 @@
+"use strict";
+
 const gulp = require("gulp");
 const browserify = require("gulp-browserify");
 const babel = require("gulp-babel");
 
-const packageJSON  = require("./package");
+const packageJSON = require("./package");
 const jscs = require("gulp-jscs");
 const jshint = require("gulp-jshint");
+
+const mocha = require("gulp-mocha");
 
 const jshintConfig = packageJSON.jshintConfig;
 jshintConfig.lookup = false;
 
 // Basic usage
 gulp.task("browser:dist", () => {
-    // Single entry point to browserify
-    gulp.src("lib3/z.js")
-        .pipe(browserify())
-        .pipe(gulp.dest("./build/"));
+	// Single entry point to browserify
+	gulp.src("lib3/z.js")
+		.pipe(browserify())
+		.pipe(gulp.dest("./build/"));
 });
 
-gulp.task("lint", () => {
-   return gulp.src([
-		"lib/**/*.js",
-		"!lib/manager/transitions/parsing/zparser.js"
-	])
+gulp.task("tests", () => {
+	gulp.src("test/**/*.js", {read: false})
+		.pipe(mocha());
+});
+
+gulp.task("code-quality", ["tests"], () => {
+	return gulp.src([
+			"lib/**/*.js",
+			"test/**/*.js",
+			"gulpfile.js",
+			"index.js",
+			"!lib/manager/transitions/parsing/zparser.js"
+		])
 		.pipe(jshint(jshintConfig))
 		.pipe(jshint.reporter("checkstyle"))
 		.pipe(jshint.reporter("fail"))
 		.pipe(jscs(packageJSON.jscsConfig))
 		.pipe(jscs.reporter());
 });
-
-gulp.task("lint-tests", () => {
-    return gulp.src([
-		"test/**/*.js"
-	])
-		.pipe(jshint(jshintConfig))
-		.pipe(jshint.reporter("checkstyle"))
-		.pipe(jshint.reporter("fail"))
-		.pipe(jscs(packageJSON.jscsConfig))
-		.pipe(jscs.reporter());
-});
-
-gulp.task("code-quality", ["lint", "lint-tests"]);
 
 gulp.task("dist", () => {
-    gulp.src("lib/z.js")
-        .pipe(babel({
-            presets: ["env"],
-            plugins: [
-                ["conditional-compilation", {
-                    DEBUG: false
-                }]
-            ]
-        }))
-        .pipe(gulp.dest("dist"));
+	gulp.src("lib/z.js")
+		.pipe(babel({
+			presets: ["env"],
+			plugins: [
+				["conditional-compilation", {
+					DEBUG: false
+				}]
+			]
+		}))
+		.pipe(gulp.dest("dist"));
 });
