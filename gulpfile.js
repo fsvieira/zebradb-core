1aa7abe51eb546e84ba9901f527b6a4ed402ed65
Filipe Vieira <sv.filipe@gmail.com>
2018-01-03 21:50:47 +0000

    add gulp code-quality task

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,56 @@
-const gulp = require('gulp');
-const browserify = require('gulp-browserify');
-const babel = require('gulp-babel');
+const gulp = require("gulp");
+const browserify = require("gulp-browserify");
+const babel = require("gulp-babel");
 
-const packageJSON  = require('./package');
-const jscs = require('gulp-jscs');
-const jshint = require('gulp-jshint');
+const packageJSON  = require("./package");
+const jscs = require("gulp-jscs");
+const jshint = require("gulp-jshint");
 
 const jshintConfig = packageJSON.jshintConfig;
 jshintConfig.lookup = false;
 
 // Basic usage
-gulp.task('browser:dist', function() {
+gulp.task("browser:dist", () => {
     // Single entry point to browserify
-    gulp.src('lib3/z.js')
+    gulp.src("lib3/z.js")
         .pipe(browserify())
-        .pipe(gulp.dest('./build/'));
+        .pipe(gulp.dest("./build/"));
 });
 
-gulp.task('lint', () => {
+gulp.task("lint", () => {
    return gulp.src([
-		'lib/**/*.js',
-		'!lib/manager/transitions/parsing/zparser.js'
+		"lib/**/*.js",
+		"!lib/manager/transitions/parsing/zparser.js"
 	])
 		.pipe(jshint(jshintConfig))
-		.pipe(jshint.reporter('checkstyle'))
-		.pipe(jshint.reporter('fail'))
+		.pipe(jshint.reporter("checkstyle"))
+		.pipe(jshint.reporter("fail"))
 		.pipe(jscs(packageJSON.jscsConfig))
 		.pipe(jscs.reporter());
 });
 
-gulp.task('lint-tests', () => {
+gulp.task("lint-tests", () => {
     return gulp.src([
-		'test/**/*.js'
+		"test/**/*.js"
 	])
 		.pipe(jshint(jshintConfig))
-		.pipe(jshint.reporter('checkstyle'))
-		.pipe(jshint.reporter('fail'))
+		.pipe(jshint.reporter("checkstyle"))
+		.pipe(jshint.reporter("fail"))
 		.pipe(jscs(packageJSON.jscsConfig))
 		.pipe(jscs.reporter());
 });
 
-gulp.task('dist', () => {
-    gulp.src('lib/z.js')
+gulp.task("code-quality", ["lint", "lint-tests"]);
+
+gulp.task("dist", () => {
+    gulp.src("lib/z.js")
         .pipe(babel({
-            presets: ['env'],
+            presets: ["env"],
             plugins: [
                 ["conditional-compilation", {
                     DEBUG: false
                 }]
             ]
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest("dist"));
 });
