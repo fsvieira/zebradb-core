0fad74347655ebc7fae70197d1d3f93b71979d4c
Filipe Vieira <sv.filipe@gmail.com>
2018-01-20 22:23:38 +0000

    change test to use new api.

--- test/zquery.js
+++ test/zquery.js
@@ -2,102 +2,92 @@
 
 const test = require("../lib/testing/test");
 
-describe("ZQuery Tests.", function () {
+describe("ZQuery Tests.", () => {
 	it("Query with single tuple constant.",
 		test(
-			`(yellow)
-            ?(yellow)`,
-
-			`?(yellow):
-                @(yellow)`
+			"(yellow)",
+			{
+				"?(yellow)" : ["@(yellow)"]
+			}
 		)
 	);
 
 	it("Query with single tuple constant/variable.",
 		test(
-			`(yellow)
-            ?(yellow)
-            ?('q)`,
-
-			`?(yellow):
-                @(yellow)
-            ?('q):
-                @(yellow)
-            `
+			"(yellow)",
+			{
+				"?(yellow)" : ["@(yellow)"],
+				"?('q)" : ["@(yellow)"]
+			}
 		)
 	);
 
 	it("Should identify variables by name, simple tuple.",
 		test(
-			`('p 'p)
-            ?(yellow 'p)`,
-
-			`?(yellow 'p):
-                @(yellow yellow)
-            `
+			"('p 'p)",
+			{
+				"?(yellow 'p)" : ["@(yellow yellow)"]
+			}
 		)
 	);
 
 	it("Should identify variables by name, inner tuples.",
 		test(
-			`('q ) (('q) ('q))
-            ?((yellow) ('p))`,
-
-			`?((yellow) ('p)):
-                @(@(yellow) @(yellow))`
+			"('q ) (('q) ('q))",
+			{
+				"?((yellow) ('p))" : ["@(@(yellow) @(yellow))"]
+			}
 		)
 	);
 
 	it("Should unify variables with tuple values.",
 		test(
-			`(blue red yellow)
-            ?('a 'b 'c)`,
-
-			`?('a 'b 'c):
-                @(blue red yellow)
-            `
+			"(blue red yellow)",
+			{
+				"?('a 'b 'c)" : ["@(blue red yellow)"]
+			}
 		)
 	);
 
 	it("Should unify simple tuples variables.",
 		test(
-			`('a 'a)
-            ?(yellow 'c)`,
-
-			`?(yellow 'c):
-                @(yellow yellow)
-            `
+			"('a 'a)",
+			{
+				"?(yellow 'c)" : ["@(yellow yellow)"]
+			}
 		)
 	);
 
 	it("Should unify simple and inner tuples variables.",
 		test(
-			`('x 'y) (('a 'a))
-            ?((yellow 'c))`,
-
-			`?((yellow 'c)):
-                @(@(yellow yellow))
-            `
+			"('x 'y) (('a 'a))",
+			{
+				"?((yellow 'c))" : ["@(@(yellow yellow))"]
+			}
 		)
 	);
 
 	it("Should unify inner tuples variables at same level.",
 		test(
-			`(yellow blue) (blue yellow) (('a 'b) ('b 'a))
-            ?((yellow 'c) (blue 'd))`,
-
-			`?((yellow 'c) (blue 'd)):
-                @(@(yellow blue) @(blue yellow))
-            `
+			`(yellow blue)
+			 (blue yellow)
+			 (('a 'b) ('b 'a))
+			`,
+			{
+				"?((yellow 'c) (blue 'd))" : [
+					"@(@(yellow blue) @(blue yellow))"
+				]
+			}
 		)
 	);
 
 	it("should handle duplicated definitions.",
 		test(
-			`('a) ('a) ?(yellow)`,
-
-			`?(yellow):
-                @(yellow)`
+			"('a) ('a)",
+			{
+				"?(yellow)" : ["@(yellow)"]
+			}
 		)
 	);
+
 });
