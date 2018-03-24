10c2e650c282f328b40430ea2959acdecf498ad4
Filipe Vieira <sv.filipe@gmail.com>
2018-03-24 11:38:54 +0000

    extract domains from branch groups

---
+++ test/domains.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const test = require("../lib/testing/test");
+
+describe("Test domain extraction.", () => {
+	it("should be a easy domain",
+		test(
+            `
+            (number 0)
+            (number 1)
+            (number 2)
+            (number 3)
+            `, [{
+				query: `?(number 'a)`,
+				results: []
+			}]
+		)
+	);
+});
+
