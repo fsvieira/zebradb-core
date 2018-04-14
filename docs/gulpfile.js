0cb029d1e402f17c36057de52c2c19be365b020f
Filipe Vieira <sv.filipe@gmail.com>
2018-04-15 00:06:12 +0100

    make website with doitblog

---
+++ docs/gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babel = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./js/index.js', { debug: true })
+    .transform(babel, {
+      presets: ["es2015"],
+      plugins: ["babel-polyfill"]
+    })
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
