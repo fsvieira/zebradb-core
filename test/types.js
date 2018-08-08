2a8c412b22f0a274215d0a0d7e0cd11e589957d7
Filipe Vieira <sv.filipe@gmail.com>
2018-08-08 23:41:58 +0100

    add types example

---
+++ test/types.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const test = require("../lib/testing/test");
+
+describe("Types Tests.", () => {
+	it("should types",
+		test(
+            `
+            (* : int -> int -> int ')
+            (* : int -> real -> real ')
+            (* : real -> int -> real ')
+            (* : real -> real -> real ')
+
+            (+ : int -> int -> int ')
+            (+ : int -> real -> real ')
+            (+ : real -> int -> real ')
+            (+ : real -> real -> real ')
+
+            (+ : string -> int ')
+            (+ : string -> real ')
+            (+ : int -> int ')
+            (+ : real -> real ')
+
+            (- : int -> int -> int ')
+            (- : int -> real -> real ')
+            (- : real -> int -> real ')
+            (- : real -> real -> real ')
+
+            (/ : int -> int -> real ')
+            (/ : int -> real -> real ')
+            (/ : real -> int -> real ')
+            (/ : real -> real -> real ')
+
+            ('x : 'y ')
+
+            nextTypes:
+                ('x : 'y 'r) -> " -> " 'y "" 'r | nextTypes,
+                ' -> "". 
+
+            firstType:
+                ('x : 'y 'r) -> "" 'y "" 'r | nextTypes,
+                ' -> "".
+
+            fn:
+                ('fn : 'fnt 'args) -> "" 'fn ": " 'args | firstType " -> " 'fnt,
+                ' -> "".
+            `,
+            [
+                {
+                    query: `?(* : int -> 'x -> real ')`,
+                    results: [
+                        "@(* : int -> real -> real ')"
+                    ]
+                },
+                {
+                    query: `
+                        ?(x : 'tx (y : 'ty
+                            (* : 'tx -> 'ty -> ' ')
+                        ))
+                    `,
+                    results: [
+                        "@(x : int @(y : int @(* : int -> int -> int ')))",
+                        "@(x : int @(y : real @(* : int -> real -> real ')))",
+                        "@(x : real @(y : {{v$149 : int real}} @(* : real -> {{v$149 : int real}} -> real ')))"
+                    ]
+                },
+                {
+                    // x * y / int
+                    query: `
+                        ?(fn : 'r (x : 'tx (y : 'ty
+                            (* : 'tx -> 'ty -> 'a (/ : 'a -> int -> 'r '))
+                        ))) | fn
+                    `,
+                    results: [
+                        "fn: int ->  int -> real",
+                        "fn: int ->  real -> real",
+                        "fn: real -> [v$209: int real] -> real"
+                    ]
+                }
+            ]
+        )
+    )
+});
