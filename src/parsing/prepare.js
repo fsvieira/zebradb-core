6cdb23b65ed800a0f1e1e1decb7a4208c6b830f5
Filipe Vieira <sv.filipe@gmail.com>
2023-10-13 15:24:15 +0100

    clean up

--- src/parsing/prepare.js
+++ src/parsing/prepare.js
@@ -32,128 +32,6 @@ const {
     }
 } = branchOps.constants;
 
-/*
-    1. Structure: 
-        * (type, op?, data?)
-
-    2. Solving Operations:
-        * variable =, in ? solvable in compile time ?
-        * Make a list of operations, to be solved at the end 
-*/
-
-/*
-function terms (ctx, t) {
-    switch (t.type) {
-        case '=': {
-            const {variable, set} = t;
-
-            if (set.type === 'set') {
-                variable.domain = set.data;
-                return terms(ctx, variable);
-            }
-            else if (set.type === 'tuple') {
-                variable.in = set;
-                return terms(ctx, variable);
-            }
-            
-            throw `Invalid Set type ${set.type}`;
-
-        }
-        case 'in': {
-            const {variable, set} = t;
-
-            if (set.type === 'cset') {
-                variable.domain = set.data;
-                return terms(ctx, variable);
-            }
-            else if (set.type === 'tuple') {
-                variable.in = set;
-                return terms(ctx, variable);
-            }
-            
-            throw `Invalid Set type ${set.type}`;
-        }
-        case 'constant': {
-            const cid = ctx.newVar(t.data);
-            ctx.variables[cid] = {type: CONSTANT, c: t.data, cid};
-
-            return cid;
-        }
-        case 'variable': {
-            const v = t.data || ctx.newVar();
-            const cid = v;
-
-            let vdata = ctx.variables[cid];
-
-            if (!vdata) {
-                vdata = ctx.variables[cid] = {type: VARIABLE, v, cid};
-            }
-
-            if (t.domain) {
-                const domains = t.domain.map(c => terms(ctx, c)).sort();
-                vdata.d = vdata.d ?
-                    vdata.d.filter(c => domains.includes(c)):
-                    domains
-                ;
-        
-                if (vdata.d.length === 0) {
-                    throw new Error("Definition has domains that cancel each other.");
-                }
-            }
-
-            if (t.in) {
-                const vin = (vdata.in || []).concat(terms(ctx, t.in));
-                vdata.in = [...new Set(vin)];
-            }
-
-            if (t.except) {
-                for (let i=0; i<t.except.length; i++) {
-                    const ecid = terms(ctx, t.except[i]);
-
-                    const args = [cid, ecid].sort();
-                    const constrainID = `_cs_${args[0]}!=${args[1]}`;
-                    const cs = {op: '!=', args, cid: constrainID };
-
-                    ctx.variables[constrainID] = cs;
-                    ctx.constraints.add(constrainID);
-
-                    vdata.e = (vdata.e || new Set());
-                    vdata.e.add(constrainID);
-                    const cdata = ctx.variables[ecid];
-
-                    if (cdata.v) {
-                        cdata.e = (cdata.e || new Set());
-                        cdata.e.add(constrainID);
-                    }
-                }
-            }
-
-            return cid;
-        }
-        case 'tuple': {
-            let body;
-            if (t.body && t.body.length) {
-                body = [];
-                for (let i=0; i<t.body.length; i++) {
-                    body.push(terms(ctx, t.body[i]));
-                }
-            }
-
-            const cid = ctx.newVar();
-            const ts = [];
-
-            for (let i=0; i<t.data.length; i++) {
-                ts.push(terms(ctx, t.data[i]));
-            }
-
-            ctx.variables[cid] = {type: TUPLE, t: ts, body, cid};
-
-            return cid;
-        }
-    }
-
-}*/
-
 function termSetExpression (ctx, s) {
     const {type, a, op, b, variable} = s;
 
@@ -173,7 +51,14 @@ function termSetExpression (ctx, s) {
 }
 
 function termSetConstrains (ctx, t) {
-    const {type, element, expression, variable, size} = t;
+    const {
+        type, 
+        element, 
+        expression, 
+        indexes,
+        variable, 
+        size
+    } = t;
 
     const cid = ctx.newVar(t);
 
@@ -416,31 +301,6 @@ function prepare (tuple) {
         globalVariable,
         constraints: ctx.constraints
     };
-
-    /*
-    const {newVar} = branchOps.varGenerator(0); 
-
-    const ctx = {
-        variables: {},
-        constraints: new Set(),
-        newVar
-    }
-
-    const root = terms(ctx, tuple);
-
-    // convert constraints set to normal array
-    for (let varname in ctx.variables) {
-        const v = ctx.variables[varname];
-
-        if (v.v && v.e) {
-            v.e = [...v.e];
-        }
-    }
-
-    // console.log(ctx.variables);
-    
-    return {variables: ctx.variables, constraints: [...ctx.constraints], root};
-    */
 }
 
 module.exports = prepare;
